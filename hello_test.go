62b3a02e649a7d8e76a5c32ca2b666c0d5c2e521
Matheus Almeida <matheus.almeida@somosdx.co>
2022-03-29 00:19:37 -0300

    test: create hello world test

---
+++ hello_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("Matheus")
+
+	want := "Hello, Matheus!"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
